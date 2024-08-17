8aee0089aad9bc0cfeb32181f84b024ad4bad946
mhmmdmngl <sorusora42@gmail.com>
2024-08-17 15:08:01 +0300

    workflow deneme

---
+++ CiftlikYonetimiYeni/Helper/JwtSettingsx.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+public static class JwtSettingsx
+{
+    public static string HashPassword(string password)
+    {
+        using (var sha1 = SHA1.Create())
+        {
+            var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return Convert.ToBase64String(hash);
+        }
+    }
+
+    public static bool VerifyPassword(string inputPassword, string hashedPassword)
+    {
+        var hashedInputPassword = HashPassword(inputPassword);
+        return hashedInputPassword == hashedPassword;
+    }
+}
